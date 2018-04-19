cfe8ee320add4171f33b4b8ebcabc9aecb705a69
JapoDeveloper <japo.developer@gmail.com>
2018-04-18 21:51:01 -0400

    First commit.

---
+++ src/main/java/co/japo/doityourself/DoItYourselfApplication.java
@@ -0,0 +1,12 @@
+package co.japo.doityourself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoItYourselfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoItYourselfApplication.class, args);
+    }
+}
